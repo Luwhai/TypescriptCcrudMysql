bea0aa266912b0150989de4bbc2bcd18474a1865
Lulu <luwhai@163.com>
2025-05-28 14:10:25 +0800

    Update dotenv.ts

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -7,5 +7,5 @@ export default {
     DATABASE_USER: process.env.DATABASE_USER || "root",
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || "123456",
     DATABASE_HOST: process.env.DATABASE_HOST || "localhost",
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 8000
 }
